b50f901ee0b7c003797aa08c126ecb01861d6a85
sang-dang812 <www.sangdang0812@gmail.com>
2025-07-02 09:35:52 +0700

    fix calendar

--- routes/calendar.js
+++ routes/calendar.js
@@ -8,11 +8,11 @@ router.post("/create",authMiddleware, async(req,res) => {
     const link = req.body.data;
     try {
         const result = await pool.query('UPDATE users SET calendar_link = $1 WHERE id = $2', [link,userId]);
-        res.status(200).json({ message: 'Memory updated successfully' });
+        res.status(200).json({ message: 'Calendar ID updated successfully' });
     }
     catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Lỗi khi cập nhật dữ liệu' });
+        res.status(500).json({ error: 'Failed to update, something went wrong' });
   }
 })
 

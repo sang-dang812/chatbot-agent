657f23c31d0324e7cbde5f4d2f34ca8bcd5f3f09
sang-dang812 <www.sangdang0812@gmail.com>
2025-07-08 16:15:05 +0700

    update send file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const userRoutes = require('./routes/user');
 const agentRoutes = require('./routes/agent');
 const calendarRoutes = require('./routes/calendar');
 const oauth2Routes = require('./routes/gmail');
+const http = require('http'); 
 
 app.use(express.static('public'));
 app.use(express.json());  
@@ -17,6 +18,9 @@ app.use('/user', userRoutes);
 app.use('/agent', agentRoutes);
 app.use('/calendar', calendarRoutes);
 app.use('/oauth2', oauth2Routes);
+
+const server = http.createServer(app);
+server.setTimeout(300000); // 5 phút (300,000 ms)
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
